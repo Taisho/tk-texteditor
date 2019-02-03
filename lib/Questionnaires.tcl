af2bf3855f839714f073971d86619f0f8492ba2f
Tichomir Mitkov <MitkovTihomir@gmail.com>
2019-02-03 13:02:38 +0200

    Adding Questionnaires module. Still in its infancy. Not usable

---
+++ lib/Questionnaires.tcl
@@ -0,0 +1,51 @@
+namespace eval Questionnaires {
+
+    proc parse_text { widget } {
+        set text [$widget get 0.1 end];
+        set SyntaxRules [dict create]
+        set SyntaxRules [parse text]
+        #set SyntaxRules "0 {start 3.0 end 3.2 tag Hashbang}"
+
+        #puts "\n\n\n"
+        #puts " (parse_text)"
+        #puts $SyntaxRules
+
+        dict for {index object} $SyntaxRules {
+             set start [dict get $object start]
+             set tag [dict get $object tag]
+
+             set end ""
+             if {[dict exists $object end]} {
+                 set end [dict get $object end]
+             }
+
+            if {$end == ""} {
+                set end end
+            }
+            $widget tag add $tag $start $end
+        }
+        # return SyntaxRules
+    }
+
+    proc list_questionnaires {textWidget} {
+
+    }
+
+    proc apply_syntax { textwidget } {
+
+        # //
+        # // NOTE when passing accross Tk widget to
+        # // a procedures, don't upvar it, as Tk
+        # // widget identifiers are not variables
+        # // (rather procedures)
+        # //
+        $textwidget configure -font regular
+        $textwidget tag configure comment -foreground #ececec
+        $textwidget tag configure variable -foreground red
+        $textwidget tag configure word_proc -foreground red
+        $textwidget tag configure DoubleQuotes -foreground red
+        $textwidget tag configure Hashbang -background #99D535 -foreground white -font bold
+        $textwidget tag configure Keyword -foreground #FB2710 -font bold
+        $textwidget tag configure Variable -foreground #FB2710 -font bold
+    }
+}
